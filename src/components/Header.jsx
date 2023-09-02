278a385860621bbee0f2559322bd7f1224844f88
ankittiwari2001 <at5542168@gmail.com>
2023-09-02 10:04:48 +0530

    Improved header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {TfiMenuAlt } from 'react-icons/tfi';
+import {IoCloseSharp} from 'react-icons/io5';
+import { RiArrowDropDownLine } from 'react-icons/ri';
 import { images } from '../constants'
 
 const NavItemsInfo = [
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Products"},
-  {name: "Contact"},
+  {name: "Home", type: "link" },
+  {name: "Articles", type: "link" },
+  {name: "Products", type: "dropdown", items: ['Hair Oils', 'Shampoo', 'Essential Oil', 'Foods'] },
+  {name: "Contact", type: "link" },
 ]
 
-const NavItem = ({name}) => {
+const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => {
+      return !curState;
+    });
+  };
+
   return (
     <li className=" relative group">
-              <a href="/" className="px-4 py-2">
-                {name}
+      {item.type === "link" ? (
+      <> 
+      <a href="/" className="px-4 py-2">
+                {item.name}
                 </a>
-              <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-                /</span>
+              <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+                /</span> 
+                </>
+                ) : (
+                <div className="flex flex-col items-center" > 
+                <button className="px-4 py-2 flex gap-x-1 items-center" onClick={toggleDropdownHandler}>
+                <span>{item.name}</span>
+                <RiArrowDropDownLine style={{ fontSize: '1.5rem' }}/>
+                </button>
+                <div className={`${dropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+                  <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden ">
+                    {item.items.map((Products) => (
+                      
+                        <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2  lg:text-dark">
+                        {Products}
+                        </a>
+                      
+                    ))}
+                  </ul>
+                </div>
+                </div>
+                
+                ) }
+             
             </li>
   );
 };
 
 const Header = () => {
+
+  const [navIsVisible, setNavIsVisible ] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) =>{
+      return !curState;
+    });  
+    };
+
+  
+
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img src={images.Logo} alt="Logo" className="h-16 w-30" />
         </div>
-        <div className="flex gap-x-9 items-center">
-          <ul className="flex gap-x-2 font-semibold">
+        <div className="lg:hidden z-50">
+          {navIsVisible ? (<IoCloseSharp className="w-6 h-6" onClick={navVisibilityHandler} />) : (<TfiMenuAlt className="w-6 h-6" onClick={navVisibilityHandler} />) }
+        </div>
+        <div className={`${navIsVisible ? "right-0" : "-right-full" } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col justify-center w-full lg:w-auto lg:justify-right lg:flex-row fixed top-[30px] bottom-0 lg:static gap-x-9 items-center` }>
+          <ul className="text-white lg:text-dark-soft items-center gap-y-5 flex flex-col lg:flex-row gap-x-2 font-semibold">
             
            {NavItemsInfo.map((item) => (
-            <NavItem key={item.name} name={item.name}/>
+            <NavItem key={item.name} item={item}/>
            ))}
           </ul>
               <button className='mt-5 md:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'> Sign In</button>
